caef52eb801afca81679e7f738e7665af852c57f
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-11 01:19:27 -0700

    init

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# source variables
+. ./.env
+
+# shutdown
+docker-compose down 2> /dev/null
+docker stop finportman-app 2> /dev/null
+docker stop finportman-api 2> /dev/null
+
+# build api
+pushd ../API
+mvn clean package -Dmaven.test.skip=true || exit 1
+docker build -t dev/finportman-api .
+popd
+
+# build app
+pushd ../portfolio_manager_frontend
+npm run build
+docker build -t dev/finportman-app .
+popd
+
+# start
+docker-compose -f docker-compose.yml up -d
+
+# clean up
+docker rmi -f $(docker images -f "dangling=true" -q)
+
+# open in browser
+chromium http://localhost:8081/register
